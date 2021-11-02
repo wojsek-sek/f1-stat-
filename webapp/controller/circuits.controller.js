15155babca9e9f2907ee48274586b75caf5e5ab8
Szymon Nolbertowicz <s.nolbertowicz@gmail.com>
2021-11-02 10:15:08 +0000

    drivers changes

--- webapp/controller/circuits.controller.js
+++ webapp/controller/circuits.controller.js
@@ -1,19 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
+	function (Controller,JSONModel) {
 		"use strict";
 
 		return Controller.extend("f1.f1stat.controller.circuits", {
 			onInit: function () {
 
+                 var oModel = new sap.ui.model.json.JSONModel();
+
+                fetch('https://ergast.com/api/f1/circuits.json?limit=78')
+                    .then(response => response.json())
+                    .then(data => oModel.setData(data));
+
+                console.log(oModel)
+
+                this.getView().setModel(oModel)
+                
             },
             handleNavButtonPress: function(){
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this); 
                 oRouter.navTo("main_view");
-            },
+            }
 		});
 	});
